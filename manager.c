b7e37007edc4bf64fb6be0f3f43c8e44cc6917fd
seoeunju02 <seoeunju5674@gmail.com>
2022-04-18 21:27:54 +0900

    update search product

--- manager.c
+++ manager.c
@@ -133,3 +133,25 @@ int loadData(Product *s){
          if(fileno==0)printf("=> 로딩 성공!\n");
          return i;
 }
+
+void searchProduct(Product *s, int count){
+     int scnt=0;
+     char search[20];
+
+     printf("검색할 제품? ");
+     scanf("%s",search);
+
+     printf("================================\n");
+     for(int i=0;i<count;i++){
+         if(s[i].price==-1) continue;
+         if(strstr(s[i].name,search)){
+             printf("%2d",i+1);
+             readProduct(s[i]);
+             scnt++;
+         }
+     }
+     if(scnt==0) printf("=> 검색된 데이터 없음!");
+     printf("\n");
+
+ }
+
